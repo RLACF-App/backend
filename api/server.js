ab773c2b9a37ca0dc4e21b32e3e63efdb6d19ec4
josh182014 <josh182014@gmail.com>
2019-10-17 14:08:09 -0600

    added eslint and linted code

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
-const server = express()
+const server = express();
 
-server.use(helmet())
-server.use(express.json())
-server.use(cors())
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 
-server.get('/', (req, res) => res.send("Server Connected. Welcome to the RLACF Volunteering App"))
+server.get('/', (req, res) => res.send('Server Connected. Welcome to the RLACF Volunteering App'));
 
-module.exports = server
+module.exports = server;
